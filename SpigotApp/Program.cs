321b47ed67efdf2c4de1b1f576b1faa2746128ab
FRNathan13 <nathan130200@gmail.com>
2018-04-21 21:48:24 -0300

    Updated base code.

--- SpigotApp/Program.cs
+++ SpigotApp/Program.cs
@@ -6,17 +6,13 @@ using System.Windows.Forms;
 
 namespace SpigotApp
 {
-	static class Program
+	public static class Program
 	{
-		/// <summary>
-		/// Ponto de entrada principal para o aplicativo.
-		/// </summary>
 		[STAThread]
 		static void Main()
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
 		}
 	}
 }
